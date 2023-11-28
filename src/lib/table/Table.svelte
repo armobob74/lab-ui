8f8073ee112ff68172e7fcbc68ea81eb83e0961e
Armon <armon@nudgex.net>
2023-11-27 16:43:28 -0800

    sourceDest table now can use a dropdown to select port

--- src/lib/table/Table.svelte
+++ src/lib/table/Table.svelte
@@ -1,12 +1,13 @@
 <script>
 	export let id;
 	import { tables_store } from '$lib/stores.js';
-	export let columns = ['name', 'number', 'quantity'];
-	export let data = [
+	let columns = ['name', 'number', 'quantity'];
+	let data = [
 		['0001', 'Apple', '22'],
 		['0002', 'Carrot', '12'],
 		['0003', 'Potato', '34']
 	];
+	export let options = {};
 	let table;
 	tables_store.subscribe((tables) => {
 		table = tables[id];
@@ -34,8 +35,18 @@
 	<tbody>
 		{#each data as row}
 			<tr>
-				{#each row as cell}
-					<td contenteditable="true" bind:innerHTML={cell} />
+				{#each row as cell, idx}
+					{#if columns[idx] in options}
+						<td
+							><select>
+								{#each options[columns[idx]] as option}
+									<option>{option}</option>
+								{/each}
+							</select></td
+						>
+					{:else}
+						<td contenteditable="true" bind:innerHTML={cell} />
+					{/if}
 				{/each}
 				<button class="btn variant-filled" on:click={() => deleteRow(row)}>X</button>
 			</tr>
@@ -45,3 +56,11 @@
 <div class="flex justify-center m-4">
 	<button class="btn variant-filled" on:click={addRow}>Add Row</button>
 </div>
+
+<style>
+	select {
+		background: none;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/routes/source-dest/+page.svelte
+++ src/routes/source-dest/+page.svelte
@@ -1,5 +1,13 @@
 <script>
 	import Table from '$lib/table/Table.svelte';
+	import { instruments_store } from '$lib/stores.js';
+	let ports = [];
+	instruments_store.subscribe((instruments) => {
+		instruments.forEach((instrument) => {
+			ports = [...ports, instrument.port];
+		});
+	});
+	let options = { Port: ports };
 </script>
 
-<Table id={'sourceDest'} />
+<Table id={'sourceDest'} {options} />

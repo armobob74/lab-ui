c9327a5d97661b6a7766ac71567c50c3ec3097bc
Armon <armon@nudgex.net>
2024-01-12 12:58:23 -0800

    excel sheets can now be read once more

--- src/routes/upload-xlsx/+page.svelte
+++ src/routes/upload-xlsx/+page.svelte
@@ -5,66 +5,39 @@
 	let files; //FileList
 	let file;
 
-	let checking_extension = false;
+	let classListSuccess = 'dark:text-success-500 text-success-800';
+	let classListError = 'text-error-500';
 	let extension_correct = false;
-	let tabcheck = false;
-	let tabs_correct = new Promise(() => {});
 	let messages = [];
 
 	async function fileUploadHandler() {
 		file = files[0];
 		checkFileExtension();
-		tabs_correct = await checkFileTabs();
 		await readTables();
 	}
 
 	function checkFileExtension() {
-		checking_extension = true;
 		if (!file.name.endsWith('.xlsx')) {
 			extension_correct = false;
 			return;
 		}
-
 		extension_correct = true;
+		let new_message = {
+			classList: extension_correct ? classListSuccess : classListError,
+			text: extension_correct
+				? '.xlsx extension confirmed, nice job!'
+				: 'please upload a .xlsx file'
+		};
+		messages = [...messages, new_message];
 		return;
-	}
-	let missing_tabs = [];
-	async function checkFileTabs() {
-		// Convert File object to ArrayBuffer
-		const arrayBuffer = await file.arrayBuffer();
-		const workbook = read(arrayBuffer);
-
-		const requiredTabs = ['Protocol', 'SourceDest', 'Steps'];
-		const fileTabs = workbook.SheetNames;
-
-		let hasRequiredTabs = true;
-		requiredTabs.forEach((tab) => {
-			if (!fileTabs.includes(tab)) {
-				missing_tabs = [...missing_tabs, tab];
-				hasRequiredTabs = false;
-			}
-		});
-
-		if (hasRequiredTabs) {
-			return true;
-		} else {
-			return false;
-		}
 	}
 
 	async function readTables() {
 		const arrayBuffer = await file.arrayBuffer();
 		const workbook = read(arrayBuffer);
-		const requiredTabs = ['Protocol', 'SourceDest', 'Steps'];
-		const tab_to_table_id = {
-			Protocol: 'protocol',
-			SourceDest: 'sourceDest',
-			Steps: 'steps'
-		};
-		requiredTabs.forEach((tab) => {
+		workbook.SheetNames.forEach((tab) => {
 			const worksheet = workbook.Sheets[tab];
 			const jsonData = utils.sheet_to_json(worksheet, { header: 1 });
-			//TODO: extract columns and rows from each tab and update the localStorageStore stuff
 			const columns = jsonData[0];
 			const data = jsonData.slice(1);
 			data.forEach((row) => {
@@ -72,36 +45,18 @@
 					row.push(''); // Add a zero to the end of the row
 				}
 			});
-			let table_id = tab_to_table_id[tab];
+			let table_id = tab;
 			updateTable(table_id, columns, data);
 		});
 	}
 
 	function updateTable(table_id, columns, data) {
 		tables_store.update((tables) => {
-			if (columns.length != tables[table_id].columns.length) {
-				messages = [
-					...messages,
-					{
-						text: `Unable to update ${table_id} because of column mismatch.`,
-						classList: 'text-error-500'
-					},
-					{
-						text: `Spreadsheet columns: "${columns.length}"`,
-						classList: 'text-error-500'
-					},
-					{
-						text: `Required columns: "${tables[table_id].columns.length}"`,
-						classList: 'text-error-500'
-					}
-				];
-				return tables;
-			}
-			messages = [
-				...messages,
-				{ text: `Updated ${table_id}`, classList: 'dark:text-success-500 text-success-800' }
-			];
-			tables[table_id].data = data;
+			messages = [...messages, { text: `Updated table: ${table_id}`, classList: classListSuccess }];
+			tables[table_id] = {
+				columns: columns,
+				data: data
+			};
 			return tables;
 		});
 	}
@@ -124,22 +79,6 @@
 			><p slot="message"><strong>Upload a file</strong> or drag and drop</p></FileDropzone
 		>
 	</div>
-	{#if checking_extension}
-		{#if extension_correct}
-			<p class="dark:text-success-500 text-success-800">.xlsx extension confirmed -- nice job!</p>
-		{:else}
-			<p class="text-error-500">Please upload a .xlsx file</p>
-		{/if}
-	{/if}
-	{#await tabs_correct then}
-		<div>
-			{#if tabs_correct}
-				<p class="dark:text-success-500 text-success-800">All required tabs are present.</p>
-			{:else}
-				<p class="text-error-500">Missing the following tabs: {missing_tabs}</p>
-			{/if}
-		</div>
-	{/await}
 	{#each messages as message}
 		<p class={message.classList}>{message.text}</p>
 	{/each}
